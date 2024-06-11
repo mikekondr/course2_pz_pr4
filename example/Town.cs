5e3094fe1c5b7073452106420bdfa536a6a12105
mikekondr <mike.kondr@gmail.com>
2024-06-12 01:09:21 +0300

    example

---
+++ example/Town.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace example
+{
+	public class Town
+	{
+        public string Name = "";
+		public string Country = "";
+		public string Region = "";
+		public int Population = 0;
+		public double YearIncome = 0;
+		public double Square = 0;
+		public bool HasPort;
+        public bool HasAirport;
+
+        public double GetYearIncomePerInhabitant()
+        {
+            return YearIncome / Population;
+        }
+    }
+}
+
